2105580a7e2601f83429ca80b4f3257547b384b6
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-04-12 13:23:23 +0300

    Command line arguments added

--- Make your own epicycle plot/fourierplot.py
+++ Make your own epicycle plot/fourierplot.py
@@ -2,9 +2,9 @@ from presentation import *
 import numpy as nm
 import matplotlib.animation
 
-time_table, x_table, y_table = create_close_loop('testimage.jpg')
+time_table, x_table, y_table = create_close_loop(sys.argv[1])
 
-order = 60
+order = int(sys.argv[2])
 coef=coef_list(time_table, x_table, y_table, order)
 print(coef)
 
@@ -22,8 +22,9 @@ ymin, ymax = ylim()
 
 anim = visualize(x_DFT, y_DFT, coef, order, space, [xmin, xmax, ymin, ymax])
 
-# plt.show()
-
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=30)
-anim.save('epicycle.mp4', writer=writer, dpi=300)
+if(sys.argv[3] == 'plot'):
+    plt.show()
+elif(sys.argv[3] == 'save'):
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=30)
+    anim.save('epicycle.mp4', writer=writer, dpi=300)

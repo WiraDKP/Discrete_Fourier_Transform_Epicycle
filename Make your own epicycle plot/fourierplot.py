101aae6f2aa04a1aa21b90d483995ca961b3b329
Daniel Holmberg <daniel.holmberg97@gmail.com>
2019-04-12 13:10:14 +0300

    Script version of jupyter notebook

---
+++ Make your own epicycle plot/fourierplot.py
@@ -0,0 +1,29 @@
+from presentation import *
+import numpy as nm
+import matplotlib.animation
+
+time_table, x_table, y_table = create_close_loop('testimage.jpg')
+
+order = 60
+coef=coef_list(time_table, x_table, y_table, order)
+print(coef)
+
+space = np.linspace(0,tau,300)
+x_DFT = [DFT(t, coef, order)[0] for t in space]
+y_DFT = [DFT(t, coef, order)[1] for t in space]
+
+fig, ax = plt.subplots(figsize=(5, 5))
+ax.plot(x_DFT, y_DFT, 'r--')
+ax.plot(x_table, y_table, 'k--')
+ax.set_aspect('equal', 'datalim')
+xmin, xmax = xlim()
+ymin, ymax = ylim()
+# fig.savefig('finalimage.jpg')
+
+anim = visualize(x_DFT, y_DFT, coef, order, space, [xmin, xmax, ymin, ymax])
+
+# plt.show()
+
+Writer = animation.writers['ffmpeg']
+writer = Writer(fps=30)
+anim.save('epicycle.mp4', writer=writer, dpi=300)
